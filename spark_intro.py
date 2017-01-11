3e12bd810ea3bc9d4b51076d4961b903d725cdba
Moses Marsh <mosesmarsh@gmail.com>
2017-01-11 09:02:10 -0800

    filled in csv splitting function

--- spark_intro.py
+++ spark_intro.py
@@ -39,7 +39,16 @@ def split_csvstring(input_string):
     >>> split_csvstring(u'a,b,0.7,"Oct 7, 2016",42,')
     ['a', 'b', '0.7', 'Oct 7, 2016', '42', '']
     """
-    pass
+
+    # we create a StringIO handler
+    fio = StringIO.StringIO(input_string)
+    # and feed that into the csv.reader library which is (probably) the best way to parse those strings
+    reader = csv.reader(fio, quotechar='"', delimiter=',',quoting=csv.QUOTE_ALL, skipinitialspace=True)
+
+    # obtains the first line of the reader (which should be the only line)
+    row_values = reader.next()
+
+    return row_values    
 
 # Part 3.2.4
 def make_row_dict(row_values, col_names, keep_keys_set):
